256168fab41b9d87fdb6773b08eda5d6938358d5
jcaps-ops <jaxon.castro@ucas-edu.net>
2026-04-09 16:50:12 +0000

    Reached a bottleneck due to errors

--- src/GUI.py
+++ src/GUI.py
@@ -1,6 +1,7 @@
 #This will be the set up for the menu using the GUI
 import tkinter as tk
 from tkinter import *
+from Functions_Edited_ForGUI import *
 import time
 
 root = tk.Tk()
@@ -17,7 +18,7 @@ Entriescatagories=tk.StringVar()
 
 
 #Here is the concept is to make pre loaded and planed areas where we will load with the diffrent labels and buttons
-def truesignup():
+def Doc2():
     btn.place_forget()
     sign.place_forget()
     root.singuplabel = tk.Label(root, text=f"This is where you would have signed up but that has not been added yet", bg="Light grey", font=("Times new roman", 14, "bold"))
@@ -38,9 +39,16 @@ def truesignup():
     root.btn1 = tk.Button(root, text="Enter",command=signingup)
     root.btn1.place(relx=.5, rely=0.7, anchor="n")
 def signingup():
+    root.singuplabel.place_forget()
+    root.SgUpLabel = tk.Label(root, text=f"", bg="Light grey", font=("Times new roman", 14, "bold"))
+    root.SgUpLabel.place(relx=.5, rely=0, anchor="n")
+
+    name=name_var.get()
+    password=passw_var.get()
+    GuiEdt_user_creator("docs/accounts.csv",name,password,root.SgUpLabel)
     #What ever do to the document
-    EnteriesGUI()
-def SignupGUI():
+    Doc4()
+def Doc3():
     
     btn.place_forget()
     sign.place_forget()
@@ -73,11 +81,11 @@ def submiting_password():
     if password == "Test2":
         root.bools += 1
     if root.bools == 2:
-        EnteriesGUI()
+        Doc4()
     else:
         root.wrg_label = tk.Label(root, text = 'Incorrect password or username', font = ('calibre',10,'bold'))
         root.wrg_label.place(relx=0.5, rely=0.2, anchor="n")
-def EnteriesGUI():
+def Doc4():
     #Clearing
     root.singuplabel.place_forget()
     root.btn1.place_forget()
@@ -89,18 +97,18 @@ def EnteriesGUI():
     root.wrg_label.place_forget()
 
     #Replacing
-    root.vent = tk.Button(root, text="View your entries")
+    root.vent = tk.Button(root, text="View your entries", command=Doc5)
     root.vent.place(relx=.35, rely=0.4, anchor="n")
-    root.aent = tk.Button(root, text="Add an entry", command=addentriesGui)
+    root.aent = tk.Button(root, text="Add an entry", command=Doc6)
     root.aent.place(relx=.45, rely=0.4, anchor="n")
-    root.dent = tk.Button(root, text="Delete an entry", command=DeleteGui)
+    root.dent = tk.Button(root, text="Delete an entry", command=Doc7)
     root.dent.place(relx=.55, rely=0.4, anchor="n")
-    root.sent = tk.Button(root, text="View a statistics", command=statisticspickerGui)
+    root.sent = tk.Button(root, text="View a statistics", command=Doc8)
     root.sent.place(relx=.65, rely=0.4, anchor="n")
     
 
 
-def statisticspickerGui():
+def Doc8():
     root.vent.place_forget()
     root.aent.place_forget()
     root.dent.place_forget()
@@ -116,15 +124,15 @@ def fixstatistcs():
     root.barbin.place_forget()
     root.piebin.place_forget()
     root.Exitbin.place_forget()
-    EnteriesGUI()
+    Doc4()
 def fixguiaddons():
     root.incomebin.place_forget()
     root.Savbin.place_forget()
     root.extbin.place_forget()
     root.expcesbin.place_forget()
 
-    EnteriesGUI()
-def addentriesGui():
+    Doc4()
+def Doc6():
     root.vent.place_forget()
     root.aent.place_forget()
     root.dent.place_forget()
@@ -139,7 +147,7 @@ def addentriesGui():
     root.extbin = tk.Button(root, text="Go back", command=fixguiaddons)
     root.extbin.place(relx=.45, rely=0.6, anchor="n")
 
-def DeleteGui():
+def Doc7():
     root.vent.place_forget()
     root.aent.place_forget()
     root.dent.place_forget()
@@ -153,15 +161,35 @@ def DeleteGui():
     root.expcesbin.place(relx=.55, rely=0.4, anchor="n")
     root.extbin = tk.Button(root, text="Go back", command=fixguiaddons)
     root.extbin.place(relx=.45, rely=0.6, anchor="n")
+
+def Doc2_1():
+    pass
+
+def Doc5():
+    root.vent.place_forget()
+    root.aent.place_forget()
+    root.dent.place_forget()
+    root.sent.place_forget()
+
+    root.incomebin = tk.Button(root, text="Income saving")
+    root.incomebin.place(relx=.35, rely=0.4, anchor="n")
+    root.Savbin = tk.Button(root, text="Savings")
+    root.Savbin.place(relx=.45, rely=0.4, anchor="n")
+    root.expcesbin = tk.Button(root, text="Expences")
+    root.expcesbin.place(relx=.55, rely=0.4, anchor="n")
+    root.extbin = tk.Button(root, text="Go back", command=fixguiaddons)
+    root.extbin.place(relx=.45, rely=0.6, anchor="n")
+    root.VEntLabel = tk.Label(root, text = '', font = ('calibre',10,'bold'))
+    #This label is where we will display the diffrent enteries 
 
     
 
 
 #Here is the starting buttons
-btn = tk.Button(root, text="Sign up", command=truesignup)
+btn = tk.Button(root, text="Sign up", command=Doc2)
 btn.place(relx=.53, rely=0.4, anchor="n")
 root.wrg_label = tk.Label(root, text = '', font = ('calibre',10,'bold'))
-sign = tk.Button(root, text="Sign in", command=SignupGUI)
+sign = tk.Button(root, text="Sign in", command=Doc3)
 sign.place(relx=.47, rely=0.4, anchor="n")
 root.exiting = tk.Button(root, text="Exit program", command=root.destroy)
 root.exiting.place(relx=.05, rely=0.05, anchor="n")
